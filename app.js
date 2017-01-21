86ab7d339c3a161d7dfcfed5aadb0dd779b72362
Mike Constanza <michael.constanza@gmail.com>
2017-01-20 21:57:14 -0500

    json fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function sendVideo(urlObject) {
     request('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ query +'&type=video&key=AIzaSyAPF280wWDUXe8i6RdW8gQ3_RnQMOP4BXk', function(error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log("Response: ", response.body.type)
-          var object = json.parse(response.body);
+          var object = JSON.parse(response.body);
             var baseURL = 'https://www.youtube.com/watch?v=';
             var video = baseURL + object.items[0].id.videoId
 
