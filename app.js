d04ec898ecced91f9ad4c7dae24f9c5626c739bb
Mike Constanza <michael.constanza@gmail.com>
2017-01-20 21:52:46 -0500

    d

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ function sendVideo(urlObject) {
         if (!error && response.statusCode == 200) {
           console.log("Response: ", response.body)
             var baseURL = 'https://www.youtube.com/watch?v=';
-            var video = baseURL + response.body.items[0].id.videoId;
+            console.log(response.body["items"], "items")
+            var video = baseURL + response.body['items'][0]['id']['videoId']
 
             slack = new Slack();
             slack.setWebhook(urlObject.response_url);
