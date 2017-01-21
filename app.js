fdd3ca005f41694fc9c954c2f99ccadfcf46e453
Mike Constanza <michael.constanza@gmail.com>
2017-01-20 21:37:55 -0500

    api fixes

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.get('/', function(request, response) {
 function sendVideo(urlObject) {
     var query = urlObject.text;
 
-    request('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ query +'&type=video&key=AIzaSyA1QnZQh1TY5flTMLL6BRMXfiHue_QMod8', function(error, response, body) {
+    request('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ query +'&type=video&key=AIzaSyAPF280wWDUXe8i6RdW8gQ3_RnQMOP4BXk', function(error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log("Response: ", response.body)
+          console.log("Response: ", response.body.items)
             var baseURL = 'https://www.youtube.com/watch?v=';
-            var video = baseURL + response.body.id.videoId;
+            var video = baseURL + response.body.items[0].id.videoId;
 
             slack = new Slack();
             slack.setWebhook(urlObject.response_url);
